efba30bf60c73952a29d389d9daa1670159ec62f
mpsgithub <manjupatwardhan@gmail.com>
2015-09-17 11:12:39 -0700

    text to voice now working across cat, subcat, topic

--- app/assets/javascripts/tts.js
+++ app/assets/javascripts/tts.js
@@ -1,8 +1,3 @@
  
-			$(document).ready(function(){
-		        $('#clickme').click(function(){
-				alert('speaking'+ $('#my_text').val());
-				responsiveVoice.speak($('#my_text').val(), "US English Female");
-				});	
-			});
+			
 		
